b0a1ca6f756324a537559488177153dd107a63f3
Stevenrej <steveo732@gmail.com>
2023-03-22 21:13:11 -0500

    postcss config updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import postcssImport from 'postcss-import';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
@@ -8,7 +9,8 @@ const __dirname = dirname(__filename);
 
 export default {
   plugins: [
+    postcssImport(),
     tailwindcss(join(__dirname, 'tailwind.config.cjs')),
-    autoprefixer,
+    autoprefixer(),
   ],
 };
