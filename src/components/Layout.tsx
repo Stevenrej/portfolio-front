739b2d5a5461b877168feefebaa1d4a1b7e01781
Stevenrej <steveo732@gmail.com>
2023-04-03 22:36:12 -0500

    tailwind styling added

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,22 @@
-import React, { ReactNode } from 'react';
-import Navigation from './Navigation';
+import React, { useEffect } from "react";
+import Navigation from "./Navigation";
 
 interface LayoutProps {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+
+  const textColor = "text-white";
+
   return (
-    <div className="layout">
-      <Navigation />
-      <main>{children}</main>
+    <div className={`fixed inset-0 overflow-y-auto border m-10 border-white-400 ${textColor}`}>
+      <div className="container mx-auto p-4 m-4">
+        <Navigation/>
+        <div className="flex justify-end">
+          <main className="w-2/3 text-right">{children}</main>
+        </div>
+      </div>
     </div>
   );
 };
