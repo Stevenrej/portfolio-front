9fba93d947fb2ffe6e6c215ea7e84e7207a0965d
Stevenrej <steveo732@gmail.com>
2023-04-07 17:16:26 -0500

    margin updated

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -34,7 +34,7 @@ const Projects: React.FC = () => {
   ];
 
   return (
-    <div className='projects mb-8'>
+    <div className='projects mb-14'>
       {projects.map((project, index) => (
         <ProjectCard
           key={index}
