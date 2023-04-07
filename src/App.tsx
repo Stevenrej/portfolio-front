1dae971b4514ede358f0c2ab07e779b4c28dda65
Stevenrej <steveo732@gmail.com>
2023-04-07 18:57:26 -0500

    custom background brought in

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Layout from './components/Layout';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import About from './pages/About';
+import CustomBackground from './assets/CustomBackground';
 import './styles.css';
 
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Layout>
+        <Layout>
+            {/* <CustomBackground> */}
+          <Routes>
+            <Route path="/" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+            {/* </CustomBackground> */}
+        </Layout>
     </Router>
   );
 };
