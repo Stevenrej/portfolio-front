33c4bd56036dff9aef07265c7b5755d95cd0943d
Stevenrej <steveo732@gmail.com>
2023-04-08 16:42:43 -0500

    motion divs brought in, tailwind updated

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 interface NavigationProps {}
 
@@ -9,61 +10,93 @@ const Navigation: React.FC<NavigationProps> = ({}) => {
       <div className="flex space-between h-8">
         <div className="flex flex-col justify-end w-1/2 ml-10">
           <div>
-            <h1 className="text-2xl font-bold text-white-800">
+            <h1 className="text-3xl font-extrabold text-white-800">
               Steven Rejdukowski
             </h1>
-            <p className="font-medium text-white-800">
+            <p className="font-medium text-white-800 ml-2 mt-2">
               Full-Stack Software Developer
             </p>
-            <p className="font-medium text-white-800">Chicago Illinois</p>
+            <p className="font-medium text-white-800 ml-2 mt-2">
+              Chicago Illinois
+            </p>
           </div>
           <div className="flex mt-4">
-            <Link
-              to="/"
-              className="text-white-800 hover:text-gray-200 mx-2 font-medium"
+            <motion.div
+              whileHover={{ scale: 1.1}}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
             >
-              Projects
-            </Link>
-            <Link
-              to="/about"
-              className="text-white-800 hover:text-gray-200 mx-2 font-medium"
+              <Link
+                to="/"
+                className="text-white-800 hover:text-gray-200 mx-2 font-bold"
+              >
+                Projects
+              </Link>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1}}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
             >
-              About
-            </Link>
-            <Link
-              to="/contact"
-              className="text-white-800 hover:text-gray-200 mx-2 font-medium"
+              <Link
+                to="/about"
+                className="text-white-800 hover:text-gray-200 mx-2 font-bold"
+              >
+                About
+              </Link>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1}}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
             >
-              Contact
-            </Link>
+              <Link
+                to="/contact"
+                className="text-white-800 hover:text-gray-200 mx-2 font-bold"
+              >
+                Contact
+              </Link>
+            </motion.div>
           </div>
         </div>
         <div className="flex justify-end w-1/2 mr-10 mt-auto mb-0">
           <div className="flex h-full">
-            <a
-              href="https://www.linkedin.com/in/steven-rejdukowski-47b879130/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white-800 hover:text-gray-200 mx-2 z-10"
+            <motion.div
+              whileHover={{ scale: 1.1}}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
             >
-              LinkedIn
-            </a>
-            <a
-              href="https://github.com/Stevenrej"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white-800 hover:text-gray-200 mx-2 z-10"
+              <a
+                href="https://www.linkedin.com/in/steven-rejdukowski-47b879130/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white-800 font-bold hover:text-gray-200 mx-2 z-20"
+              >
+                LinkedIn
+              </a>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1}}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
             >
-              GitHub
-            </a>
-            <a
-              href="https://docs.google.com/document/d/15VKSgodzYJZAe4ZJACTgC0Xtnjn-EXsU8yn6rk659d8/edit?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white-800 hover:text-gray-200 mx-2 z-10"
+              <a
+                href="https://github.com/Stevenrej"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white-800 font-bold hover:text-gray-200 mx-2 z-20"
+              >
+                GitHub
+              </a>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1}}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
             >
-              Resume
-            </a>
+              <a
+                href="https://docs.google.com/document/d/15VKSgodzYJZAe4ZJACTgC0Xtnjn-EXsU8yn6rk659d8/edit?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white-800 font-bold hover:text-gray-200 mx-2 z-20"
+              >
+                Resume
+              </a>
+            </motion.div>
           </div>
         </div>
       </div>
