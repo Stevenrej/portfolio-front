cefb719d02ecd112144e8d1de741acc660f132fa
Stevenrej <steveo732@gmail.com>
2023-04-10 18:53:03 -0500

    setCurrentPage function passed through component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,16 +3,18 @@ import Navigation from "./Navigation";
 
 interface LayoutProps {
   children: React.ReactNode;
+  setCurrentPage: React.Dispatch<React.SetStateAction<"projects" | "about" | "contact">>;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+
+const Layout: React.FC<LayoutProps> = ({ children, setCurrentPage }) => {
 
   const textColor = "text-white";
 
   return (
     <div className={`fixed inset-0 overflow-y-auto border m-10 border-white-400 ${textColor}`} id="scroll">
       <div className="container ml-auto p-1 m-1">
-        <Navigation/>
+        <Navigation setCurrentPage={setCurrentPage} />
         <div className="flex justify-end">
           <main className="w-3/4 text-right">{children}</main>
         </div>
