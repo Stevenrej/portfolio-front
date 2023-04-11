21db97df25913c03fa3db26401266f7f45c524e3
Stevenrej <steveo732@gmail.com>
2023-04-10 22:46:46 -0500

    styling updated, bullet now shows on selected page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
 interface NavigationProps {
-  setCurrentPage: React.Dispatch<React.SetStateAction<"projects" | "about" | "contact">>;
+  setCurrentPage: React.Dispatch<
+    React.SetStateAction<"projects" | "about" | "contact">
+  >;
+  currentPage: "projects" | "about" | "contact";
 }
 
-const Navigation: React.FC<NavigationProps> = ({ setCurrentPage }) => {
-
+const Navigation: React.FC<NavigationProps> = ({
+  setCurrentPage,
+  currentPage,
+}) => {
   const handleLinkClick = (page: "projects" | "about" | "contact") => {
     setCurrentPage(page);
     const container = document.getElementById("scroll");
@@ -20,7 +25,7 @@ const Navigation: React.FC<NavigationProps> = ({ setCurrentPage }) => {
   };
 
   return (
-    <nav className="fixed bottom-0 inset-x-0 p-4 mb-10 mx-auto w-full text-white-800">
+    <nav className="fixed bottom-0 inset-x-0 p-4 mb-10 mx-auto w-full text-white-800" style={{ zIndex: 100 }}>
       <div className="flex space-between h-8">
         <div className="flex flex-col justify-end w-1/2 ml-10">
           <div>
@@ -41,10 +46,15 @@ const Navigation: React.FC<NavigationProps> = ({ setCurrentPage }) => {
             >
               <Link
                 to="/"
-                className="text-white-800 hover:text-gray-200 mx-2 font-bold"
-                onClick={() => {handleLinkClick("projects")}}
+                className="text-white-800 hover:text-gray-200 mx-2 ml-2 font-bold"
+                onClick={() => {
+                  handleLinkClick("projects");
+                }}
               >
                 Projects
+    
+                  <span className="inline-block ml-2 dash"  style={{ opacity: currentPage === "projects" ? 1 : 0 }}>&#x2022;</span>
+      
               </Link>
             </motion.div>
             <motion.div
@@ -54,9 +64,14 @@ const Navigation: React.FC<NavigationProps> = ({ setCurrentPage }) => {
               <Link
                 to="/about"
                 className="text-white-800 hover:text-gray-200 mx-2 font-bold"
-                onClick={() => {handleLinkClick("about")}}
+                onClick={() => {
+                  handleLinkClick("about");
+                }}
               >
                 About
+
+                  <span className="inline-block ml-2 dash"  style={{ opacity: currentPage === "about" ? 1 : 0 }}>&#x2022;</span>
+              
               </Link>
             </motion.div>
             <motion.div
@@ -66,15 +81,20 @@ const Navigation: React.FC<NavigationProps> = ({ setCurrentPage }) => {
               <Link
                 to="/contact"
                 className="text-white-800 hover:text-gray-200 mx-2 font-bold"
-                onClick={() => {handleLinkClick("contact")}}
+                onClick={() => {
+                  handleLinkClick("contact");
+                }}
               >
                 Contact
+  
+                  <span className="inline-block ml-2 dash"  style={{ opacity: currentPage === "contact" ? 1 : 0 }}>&#x2022;</span>
+    
               </Link>
             </motion.div>
           </div>
         </div>
         <div className="flex justify-end w-1/2 mr-10 mt-auto mb-0">
-          <div className="flex h-full">
+          <div className="flex h-full" >
             <motion.div
               whileHover={{ scale: 1.1 }}
               transition={{ type: "spring", stiffness: 400, damping: 10 }}
@@ -83,7 +103,7 @@ const Navigation: React.FC<NavigationProps> = ({ setCurrentPage }) => {
                 href="https://www.linkedin.com/in/steven-rejdukowski-47b879130/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-white-800 font-bold hover:text-gray-200 mx-2 z-20"
+                className="text-white-800 font-bold hover:text-gray-200 mx-5 "
               >
                 LinkedIn
               </a>
@@ -96,7 +116,7 @@ const Navigation: React.FC<NavigationProps> = ({ setCurrentPage }) => {
                 href="https://github.com/Stevenrej"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-white-800 font-bold hover:text-gray-200 mx-2 z-20"
+                className="text-white-800 font-bold hover:text-gray-200 mx-5 "
               >
                 GitHub
               </a>
@@ -109,7 +129,7 @@ const Navigation: React.FC<NavigationProps> = ({ setCurrentPage }) => {
                 href="https://docs.google.com/document/d/15VKSgodzYJZAe4ZJACTgC0Xtnjn-EXsU8yn6rk659d8/edit?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-white-800 font-bold hover:text-gray-200 mx-2 z-20"
+                className="text-white-800 font-bold hover:text-gray-200 mx-5 "
               >
                 Resume
               </a>
