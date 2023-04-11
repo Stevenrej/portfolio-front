63eb21ed5f6149a5a924b5bf4ca9f9771f0cda45
Stevenrej <steveo732@gmail.com>
2023-04-10 22:46:19 -0500

    currentPage state brought in and passed in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,17 +4,18 @@ import Navigation from "./Navigation";
 interface LayoutProps {
   children: React.ReactNode;
   setCurrentPage: React.Dispatch<React.SetStateAction<"projects" | "about" | "contact">>;
+  currentPage: "projects" | "about" | "contact";
 }
 
 
-const Layout: React.FC<LayoutProps> = ({ children, setCurrentPage }) => {
+const Layout: React.FC<LayoutProps> = ({ children, setCurrentPage, currentPage }) => {
 
   const textColor = "text-white";
 
   return (
     <div className={`fixed inset-0 overflow-y-auto border m-10 border-white-400 ${textColor}`} id="scroll">
       <div className="container ml-auto p-1 m-1">
-        <Navigation setCurrentPage={setCurrentPage} />
+        <Navigation setCurrentPage={setCurrentPage} currentPage={currentPage} />
         <div className="flex justify-end">
           <main className="w-3/4 text-right">{children}</main>
         </div>
