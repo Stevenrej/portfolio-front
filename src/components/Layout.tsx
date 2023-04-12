0820a376ac870da1e2e0f3b3c860169c552ba79e
Stevenrej <steveo732@gmail.com>
2023-04-12 12:17:40 -0500

    border area updated

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,7 +13,7 @@ const Layout: React.FC<LayoutProps> = ({ children, setCurrentPage, currentPage }
   const textColor = "text-white";
 
   return (
-    <div className={`fixed inset-0 overflow-y-auto border m-10 border-white-400 ${textColor}`} id="scroll">
+    <div className={`fixed inset-0 overflow-y-auto border m-6 border-white-400 ${textColor}`} id="scroll">
       <div className="container ml-auto p-1 m-1">
         <Navigation setCurrentPage={setCurrentPage} currentPage={currentPage} />
         <div className="flex justify-end">
