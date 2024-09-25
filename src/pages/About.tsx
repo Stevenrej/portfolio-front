90368880ead4c5582a3dec376d223f597208a1de
Stevenrej <steveo732@gmail.com>
2024-09-24 20:01:22 -0500

    about text updated

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -21,34 +21,43 @@ const About: React.FC<Props> = ({ currentPage }) => {
                 <h1 className="text-2xl font-bold mb-4 text-gray-300">
                   Full-stack software developer based in Chicago, IL,
                   specializing in creating responsive, user-friendly web
-                  applications with a focus on e-commerce solutions. With over 7
-                  years of experience in eCommerce management and a deep
-                  understanding of front-end and back-end technologies, I
+                  applications with a focus on e-commerce solutions. With over 8
+                  years of experience in eCommerce management and development, I
                   possess a unique blend of technical expertise and
-                  customer-centric mindset that enable me to design and develop
+                  customer-centric mindset that enables me to design and develop
                   high-quality web experiences.
                 </h1>
                 <div>
                   <p className="mb-4">
                     I am passionate about continuous learning and
-                    problem-solving, which is why I pursued a certificate in
+                    problem-solving, which led me to pursue a certificate in
                     full-stack JavaScript development through Code Fellows. This
-                    program allowed me to sharpen my skills in various
-                    languages.
+                    program allowed me to sharpen my skills in various languages
+                    and technologies.
                   </p>
                   <p className="mb-4">
-                    Thanks to my proficiency in various tools and frameworks, I
-                    feel well-equipped to tackle even the most complex software
-                    projects. I am excited to continue honing my skills and
-                    using them to develop innovative solutions in the field of
-                    software development.
+                    In my recent role at oBundle, I've further expanded my
+                    expertise by building affiliate program applications,
+                    creating custom order tools, and developing tailored
+                    e-commerce solutions using platforms like BigCommerce. I've
+                    collaborated closely with designers to implement
+                    pixel-perfect, responsive interfaces and integrated complex
+                    backend functionality to meet specific client needs.
+                  </p>
+                  <p className="mb-4">
+                    My proficiency in various tools and frameworks, combined
+                    with my recent experience in developing custom checkout
+                    solutions and e-commerce themes, equips me to tackle complex
+                    software projects efficiently. I'm excited to leverage these
+                    skills to continue developing innovative solutions in the
+                    field of software development.
                   </p>
                 </div>
               </div>
               <div className="col-span-1 md:col-span-2">
                 <div className="mb-8">
                   <h3 className="text-xl font-semibold mb-2">Availability</h3>
-                  <p>April 2023 -</p>
+                  <p>October 2024 -</p>
                   <p>Remotely or in Chicagoland Area</p>
                 </div>
                 <div>
@@ -67,36 +76,38 @@ const About: React.FC<Props> = ({ currentPage }) => {
                   </h4>
                   <ul className="list-none list-inside grid grid-cols-2 text-gray-300">
                     <li>React</li>
-                    <li>Tailwind CSS</li>
-                    <li>Sass</li>
-                    <li>PostCSS</li>
-                    <li>Vite</li>
-                    <li>GitHub</li>
-                    <li>Git</li>
-                    <li>Express.js</li>
                     <li>Node</li>
+                    <li>Git</li>
+                    <li>GitHub</li>
+                    <li>Express.js</li>
                     <li>Next.js</li>
-                    <li>Redux.js</li>
-                    <li>React Native</li>
-                    <li>Axios</li>
                     <li>PostgreSQL</li>
                     <li>MongoDB</li>
-                    <li>Socket.io</li>
-                    <li>AWS Lambda</li>
-                    <li>AWS DynamoDB</li>
-                    <li>AWS S3</li>
-                    <li>AWS Elastic Beanstalk</li>
-                    <li>AWS EC2</li>
-                    <li>Netlify</li>
-                    <li>Heroku</li>
-                    <li>Bootstrap</li>
-                    <li>CodeSandbox</li>
+                    <li>Redux.js</li>
+                    <li>Tailwind CSS</li>
+                    <li>AWS</li>
+                    <li>Firebase</li>
+                    <li>Docker</li>
                     <li>VS Code</li>
-                    <li>Render</li>
+                    <li>Postman</li>
+                    <li>Axios</li>
+                    <li>Figma</li>
+                    <li>Vite</li>
+                    <li>React Native</li>
+                    <li>Sass</li>
+                    <li>Socket.io</li>
+                    <li>Bootstrap</li>
+                    <li>Netlify</li>
+                    <li>BigCommerce</li>
+                    <li>Shopify</li>
                     <li>Sequelize</li>
                     <li>Expo</li>
-                    <li>Shopify</li>
+                    <li>PostCSS</li>
                     <li>Mantine</li>
+                    <li>Render</li>
+                    <li>CodeSandbox</li>
+                    <li>Gadget</li>
+                    <li>WebDAV</li>
                   </ul>
                 </div>
               </div>
